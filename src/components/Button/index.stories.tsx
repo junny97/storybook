0d58accbb05b750f9e73a5bdbf087485cd8407c9
junny97 <pcohad12@gmail.com>
2024-06-14 14:01:00 +0900

    Add Text Component & Types

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -5,7 +5,7 @@ const meta: Meta<typeof Button> = {
   title: 'Components/Button',
   component: Button,
   parameters: {
-    layout: 'centered',
+    layout: 'padded',
   },
   tags: ['autodocs'],
 };
---
+++ src/components/Text/Text.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text } from './Text';
+
+const meta: Meta<typeof Text> = {
+  title: 'Components/Text',
+  component: Text,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    as: 'h1',
+    children: 'how about your today',
+  },
+};
