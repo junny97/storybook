e41ba26c759047fc38d2bc085d3f79ae5102bc7e
junny97 <pcohad12@gmail.com>
2024-06-14 13:09:07 +0900

    Add new dependencies and styles for Button component

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Button from '.';
+import { Button } from '.';
 
 const meta: Meta<typeof Button> = {
   title: 'Components/Button',
@@ -14,29 +14,24 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Solid: Story = {
   args: {
-    children: 'Click me',
+    variant: 'solid',
+    size: 'lg',
+    children: 'Solid Button',
   },
 };
 
-// export const Solid: Story = {
-//   args: {
-//     variant: 'solid',
-//     children: 'Button',
-//   },
-// };
+export const Outline: Story = {
+  args: {
+    variant: 'outline',
+    children: 'OutLine Button',
+  },
+};
 
-// export const Outline: Story = {
-//   args: {
-//     variant: 'outline',
-//     children: 'Button',
-//   },
-// };
-
-// export const Ghost: Story = {
-//   args: {
-//     variant: 'ghost',
-//     children: 'Button',
-//   },
-// };
+export const Ghost: Story = {
+  args: {
+    variant: 'ghost',
+    children: 'Ghost Button',
+  },
+};
