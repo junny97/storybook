ae8af751dc7ccd13b2be76ea8120b03a64161505
junny97 <pcohad12@gmail.com>
2024-06-14 11:07:52 +0900

    Install TailwindCSS & Storybook

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,12 @@
-export default function index() {
-  return <button className='text-blue-500'>Button</button>;
+import { ComponentProps } from 'react';
+
+type ButtonProps = ComponentProps<'button'>;
+
+export default function Button({ ...props }: ButtonProps) {
+  return (
+    <button
+      className='text-blue-500 border border-gray-200'
+      {...props}
+    ></button>
+  );
 }
